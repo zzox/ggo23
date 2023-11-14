1c9d5c31d48297b9f7d7eefa4b2418c4b2375912
Tyler McCarthy <tweedlitweedli@gmail.com>
2023-11-13 18:42:07 -0600

    positioning sprites, outline on worlditemsprites

--- src/game/objects/WorldItemSprite.hx
+++ src/game/objects/WorldItemSprite.hx
@@ -6,26 +6,37 @@ import core.Types;
 import kha.Image;
 import kha.graphics2.Graphics;
 
-class WorldItemSprite extends Sprite {
-    // the offset of y position of where this will actually be drawn
-    // NOTE: offset.y deals with the `body` offset from the sprite origin,
-    // this is different
-    public var yOffset:Int;
+final dirDiffs = [new IntVec2(1, 0), new IntVec2(-1, 0)];
+// final dirDiffs = [new IntVec2(1, 0), new IntVec2(-1, 0), new IntVec2(0, 1), new IntVec2(0, -1)];
 
-    public function new (pos:Vec2, image:Image, size:IntVec2, color:Int, yOffset:Int = 20) {
+class WorldItemSprite extends Sprite {
+    static inline final Y_OFFSET:Int = 26;
+
+    public function new (pos:Vec2, image:Image, size:IntVec2, color:Int) {
         super(pos, image, size);
 
         this.color = color;
-        this.yOffset = yOffset;
     }
 
     override function render (g2:Graphics, cam:Camera) {
-        for (c in _children) {
-            c.setPosition(x, y - yOffset);
-        }
+        // for (c in _children) {
+        //     c.setPosition(x, y - yOffset);
+        // }
 
         final oldY = y;
-        y -= yOffset;
+        final oldX = x;
+        final oldColor = color;
+        final newY = y - Y_OFFSET;
+
+        for (diff in dirDiffs) {
+            setPosition(oldX + diff.x, newY + diff.y);
+            color = 0xff000000;
+            super.render(g2, cam);
+        }
+
+        x = oldX;
+        y = newY;
+        color = oldColor;
         super.render(g2, cam);
         y = oldY;
     }
